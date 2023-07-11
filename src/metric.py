9e5f37e1037e639a69869bbbcf5ba662848bf16b
Patrick <patrick.madlindl@mytum.de>
2023-07-11 10:10:39 +0000

    Added weighted mean M3C2

--- src/metric.py
+++ src/metric.py
@@ -7,11 +7,60 @@ import os, sys
 import helper, classes
 
 EVERY_NTH = 10000
-MIN_POINTS = 10
+MIN_POINTS = 20
 
+CLASS_NUM_TO_WEIGHT = {
+    0: 0.1,     # Road
+    1: 0.1,     # Ground
+    2: 0.2,    # Wall Surface
+    3: 0.15,    # Roof Surface
+    4: 0.15,    # Doors
+    5: 0.15,    # Windows
+    6: 0.15,     # Building Installation
+}
 
-def compute_metric(dir_m3c2_real, dir_m3c2_synth, c2c_distance):
-    return 0
+def compute_metric(real_pc_path, synth_pc_path, c2c_distance=None):
+    print("Computing Class-Wise M3C2 Distances...")
+    class_wise_distances_medians, class_wise_distances_all, class_wise_uncertainties_all, skipped_classes = m3c2_class_wise(real_pc_path, synth_pc_path)
+    classes_to_ignore = []
+    print("M3C2 Medians for each class:")
+    for class_number, median_distance in class_wise_distances_medians:
+        class_idx = list(classes.CLASSES_FOR_M3C2_REAL.keys())[class_number]
+        class_name = classes.CLASSES_FOR_M3C2_REAL[class_idx]
+        num_distances = len(class_wise_distances_all[class_number])
+        nan_ratio = np.sum(np.isnan(class_wise_distances_all[class_number]))/num_distances
+        print(f"\tClass {class_name} ({class_number}):\tMedian distance = {median_distance},\t#distances: {num_distances},\tNan-Ratio: {nan_ratio}")
+        if np.isnan(median_distance):
+            classes_to_ignore.append(class_number)
+    
+    print("M3C2 Ignored classes (NaN distances only):")
+    for class_number in classes_to_ignore:
+        class_idx = list(classes.CLASSES_FOR_M3C2_REAL.keys())[class_number]
+        class_name = classes.CLASSES_FOR_M3C2_REAL[class_idx]
+        print(f"\tClass {class_name} ({class_number})")
+
+    print("M3C2 Skipped classes (too few points):")
+    for class_number in skipped_classes:
+        class_idx = list(classes.CLASSES_FOR_M3C2_REAL.keys())[class_number]
+        class_name = classes.CLASSES_FOR_M3C2_REAL[class_idx]
+        print(f"\tClass {class_name} ({class_number})")
+        classes_to_ignore.append(class_number)
+
+    
+    # We need to remove weights for classes where distance is NaN and ignored classes. Renormalize weights (sum==1)
+    weights = np.array([CLASS_NUM_TO_WEIGHT.get(class_number) for class_number in CLASS_NUM_TO_WEIGHT if class_number not in classes_to_ignore])
+    weights = weights / np.sum(weights)
+
+    mean_m3C2 = 0.0
+    weight_idx = 0
+
+    for class_number, median_distance in class_wise_distances_medians:
+        if class_number not in classes_to_ignore:
+            #print(f"\tAdding to Mean: Class Number {class_number}, Weight {weights[weight_idx]}, Median {median_distance}")
+            mean_m3C2 += weights[weight_idx] * np.abs(median_distance)
+            weight_idx += 1
+
+    return mean_m3C2
 
 def laspy_to_np_array(laspy_points):
     x = laspy_points['x']
@@ -41,9 +90,9 @@ def class_split_pc(points_all_classes, type=None):
 
 
 def m3c2_class_wise(real_pc_path, synth_pc_path):
-    print('Reading & preparing data')
+    print('\tM3C2 Reading & preparing data')
     real_points_all_classes, synth_points_all_classes = helper.import_and_prepare_point_clouds(real_pc_path, synth_pc_path, shift_real=True, flip_synth=True, crop=True)
-    print('Splitting data')
+    print('\tM3C2 Splitting data')
     real_points_class_wise = class_split_pc(real_points_all_classes, type='real')
     synth_points_class_wise = class_split_pc(synth_points_all_classes, type='synth')
 
@@ -55,12 +104,12 @@ def m3c2_class_wise(real_pc_path, synth_pc_path):
     class_wise_uncertainties_all = []
     skipped_classes = []
 
-    print('Calculating distances...')
-    for class_number in tqdm(range(len(real_points_class_wise))):
+    print('\tM3C2 Calculating distances...')
+    for class_number in tqdm(range(len(real_points_class_wise)), bar_format='\t{l_bar}{bar}'):
         
         if len(real_points_class_wise[class_number]) < MIN_POINTS or len(synth_points_class_wise[class_number]) < MIN_POINTS:
             # not enough points for meaningful calculation
-            print("Class {} has not enough points and is skipped".format(classes.CLASSES_FOR_M3C2_REAL[list(classes.CLASSES_FOR_M3C2_REAL.keys())[class_number]]))
+            print("\tM3C2 Class {} has not enough points and is skipped".format(classes.CLASSES_FOR_M3C2_REAL[list(classes.CLASSES_FOR_M3C2_REAL.keys())[class_number]]))
             skipped_classes.append(class_number)
         else:
             # m3c2 needs special epoch data type, timestamp is optional 
@@ -101,19 +150,8 @@ if __name__ == "__main__":
         real_pc_path = sys.argv[1]
         synth_pc_path = sys.argv[2]
     
-    class_wise_distances_medians, class_wise_distances_all, class_wise_uncertainties_all, skipped_classes = m3c2_class_wise(real_pc_path, synth_pc_path)
+    mean_m3C2 = compute_metric(real_pc_path, synth_pc_path, c2c_distance=None)
+    print(mean_m3C2)
     
-    print("Medians for each class:")
-    for class_number, median_distance in class_wise_distances_medians:
-        class_idx = list(classes.CLASSES_FOR_M3C2_REAL.keys())[class_number]
-        class_name = classes.CLASSES_FOR_M3C2_REAL[class_idx]
-        num_distances = len(class_wise_distances_all[class_number])
-        nan_ratio = np.sum(np.isnan(class_wise_distances_all[class_number]))/num_distances
-        print(f"Class {class_name} ({class_number}):\tMedian distance = {median_distance},\t#distances: {num_distances},\tNan-Ratio: {nan_ratio}")
-    print("Skipped classes:")
-    for class_number in skipped_classes:
-        class_idx = list(classes.CLASSES_FOR_M3C2_REAL.keys())[class_number]
-        class_name = classes.CLASSES_FOR_M3C2_REAL[class_idx]
-        print(f"Class {class_name} ({class_number})")
 
     
